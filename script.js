321cd8ba07b7601b33f6e29d709ef5ee578bf14d
Pooja26-maker <vainavi.s.2016@gmail.com>
2026-01-20 20:41:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Smooth scroll for nav links
+const navLinks = document.querySelectorAll('header nav ul li a');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    const targetId = link.getAttribute('href').substring(1);
+    const targetSection = document.getElementById(targetId);
+    targetSection.scrollIntoView({ behavior: 'smooth' });
+  });
+});
+
